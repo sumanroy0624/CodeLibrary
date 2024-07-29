71664f8d0a315ac488fdefa3ba895f948bf83111
Suman Roy <138618510+sumanroy0624@users.noreply.github.com>
2024-07-30 02:51:23 +0530

    Create Dice_Combination.cpp

---
+++ Dynamic Programming CSES/Dice_Combination.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define print_set(s) for(auto it=s.begin(); it!=s.end(); it++){cout<<*it<<" ";}cout<<endl;
+#define sv(v) sort(v.begin(),v.end())
+#define rv(v) reverse(v.begin(),v.end())
+#define in_vect(v) for(ll i=0;i<v.size();i++){cin>>v[i];}
+#define tc(t) int t; cin >> t; while (t--)
+#define ll long long
+#define lli long long int
+#define pb push_back
+#define pob pop_back
+#define ln() cout<<endl
+#define cn cout<<"NO"<<endl
+#define cy cout<<"YES"<<endl
+#define ss second
+#define ff first
+#define all(v) v.begin(), v.end()
+#define pv(v) for(ll i=0; i<v.size(); i++){cout<<v[i]<<" ";}cout<<endl;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define vi vector<ll>
+#define maxpq priority_queue<int>
+#define minpq priority_queue<int,vector<int>,greater<int>>
+int gcd(int a, int b) {if (a == 0) return b; return gcd(b % a, a);}
+int lcm(int a, int b) {return a * b / gcd(a, b);}
+int max(int a, int b) {if (a > b) return a; else return b;}
+int min(int a, int b) {if (a < b) return a; else return b;}
+bool isPrime(ll x) {for (int i = 2; i * i <= x; i++) if (x % i == 0) return false; return true;}
+const int N = 1e6;
+vector<bool>prime(N + 1, true);
+void sieve() {prime[0] = false; prime[1] = false; for (int i = 2; i * i <= N; i++) {if (prime[i] == true) {for (int j = i * i; j <= N; j += i) {prime[j] = false;}}}}
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>ordered_set;
+//find_by_order, order_of_key
+ll mod = 1e9 + 7;
+ 
+void solve() {
+	ll n;
+	cin >> n;
+	vector<ll>dp(n + 1, 0);
+	dp[0] = 1;
+	for (int i = 1; i <= n; i++) {
+		ll val = 0;
+		for (int j = 1; j <= 6; j++) {
+			if (i - j >= 0) {
+				val += dp[i - j] % mod;
+			}
+		}
+		dp[i] = val % mod;
+	}
+	cout << dp[n] << endl;
+}
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	// tc(t) {
+	solve();
+	// }
+	return 0;
+}
